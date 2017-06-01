864607c874e570021c3e70e9af801f86330ed79a
Ibrahim ZEHHAF <izehhaf@keley-live.com>
2017-06-01 02:08:01 +0200

    modal resposive fix bug

---
+++ src/AppBundle/Resources/public/js/delete_modal.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".delete-button").click(function (event) {
+        var modalButton = $('#delete-modal .modal-delete-button');
+        var href = modalButton.attr("href").replace('_id_', $(this).data('id'));
+        modalButton.attr("href", href);
+        $('#delete-modal').modal();
+    });
+});
