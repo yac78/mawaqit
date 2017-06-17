b9831649193cfda4ac0e55ce3ecba32cf3283a3c
Ibrahim ZEHHAF <izehhaf@keley-live.com>
2017-06-17 13:54:40 +0200

    add phone in support

--- src/AppBundle/Resources/public/js/iframe_modal.js
+++ src/AppBundle/Resources/public/js/iframe_modal.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(".iframe-button").click(function (event) {
-        var iframe =  '<iframe src="' + $(this).data('url') + '" frameborder="0" scrolling="no" style="width: 360px; height: 570px;"></iframe>'
+        var iframe =  '<iframe src="' + $(this).data('url') + '" frameborder="0" scrolling="no" style="width: 360px; height: 600px;"></iframe>'
         $('#iframe-modal #iframe-text').text(iframe);
         $('#iframe-modal #iframe-html').html(iframe);
         $('#iframe-modal').modal();
