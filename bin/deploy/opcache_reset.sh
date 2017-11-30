41aa1944a706ec3cfacfab25c177ab18212fdc4f
Ibrahim ZEHHAF <izehhaf@keley-live.com>
2017-12-01 00:06:42 +0100

    deploy

--- bin/deploy/opcache_reset.sh
+++ bin/deploy/opcache_reset.sh
@@ -2,5 +2,5 @@
 WEBDIR=/var/www/mawaqit/prod/current/web/
 RANDOM_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)
 echo "<?php var_dump(opcache_reset());" > ${WEBDIR}${RANDOM_NAME}.php
-curl http://localhost:443/${RANDOM_NAME}.php
+curl -s https://localhost/${RANDOM_NAME}.php > /dev/null
 rm ${WEBDIR}${RANDOM_NAME}.php
