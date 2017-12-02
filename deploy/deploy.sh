c5d7cd4b8633d52fdc3808fd08b34eaea45ddce0
Ibrahim ZEHHAF <izehhaf@keley-live.com>
2017-12-02 12:25:10 +0100

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,14 +8,19 @@ if [ $# -lt 2 ]; then
     exit;
 fi
 
-server="137.74.45.69"
-port="1983"
-
 env=$1
 tag=$2
 
-git tag $tag -m "new release $2" || true
-git push
-git push origin $tag
+echo -n "Are you sur you want to deploy $tag to $env (y/n)? "
+read answer
 
-ssh -p $port mawaqit@$server 'bash -s' < deploy/install.sh $env $tag
+if echo "$answer" | grep -iq "^y" ;then
+    server="137.74.45.69"
+    port="1983"
+
+    git tag $tag -m "new release $2" || true
+    git push
+    git push origin $tag
+
+    ssh -p $port mawaqit@$server 'bash -s' < deploy/install.sh $env $tag
+fi
