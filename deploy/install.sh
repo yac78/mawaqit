e0a630c5d4af2f09ec3171023d4de4e80488fe11
Ibrahim ZEHHAF <izehhaf@keley-live.com>
2017-12-08 21:23:15 +0100

    deploy

--- deploy/install.sh
+++ deploy/install.sh
@@ -51,8 +51,8 @@ cd $envDir && rm current || true && ln -s $tag current
 echo "Force reload mosques"
 mysql -u mawaqit -p`cat $sharedDir/dbpwd` mawaqit_$env < $repoDir/deploy/update.sql
 
-echo "Reset opcache => $repoDir/deploy/opcache_reset.sh $env"
-$repoDir/deploy/opcache_reset.sh $env
+echo "Reset opcache => $repoDir/deploy/opcache_reset.sh"
+$repoDir/deploy/opcache_reset.sh
 
 echo "Deleting old releases, keep 3 latest"
 rm -rf `ls -t  | tail -n +5` || true
--- deploy/opcache_reset.sh
+++ deploy/opcache_reset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-WEBDIR=/var/www/mawaqit/$1/current/web/
+WEBDIR=/var/www/mawaqit/prod/current/web/
 FILE_NAME="opcacheReset.php"
 echo "<?php opcache_reset();" > ${WEBDIR}${FILE_NAME}
-curl -ks https://localhost/${FILE_NAME} > /dev/null
+curl -s https://mawaqit.net/${FILE_NAME} > /dev/null
 rm ${WEBDIR}${FILE_NAME}
