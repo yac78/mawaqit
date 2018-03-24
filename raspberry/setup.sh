80cf6410bc67087a9c72132c13ac5ed60f4584ab
binary010100 <ibrahim.zehhaf.pro@gmail.com>
2018-03-24 23:29:29 +0100

    Update setup.sh

--- raspberry/setup.sh
+++ raspberry/setup.sh
@@ -82,7 +82,7 @@ service nginx restart
 
 # create files on Desktop
 echo "https://mawaqit.net/fr/mosquee-essunna-houilles" >  /home/pi/Desktop/online_site.txt
-echo "http://localhost" >  /home/pi/Desktop/local_site.txt
+echo "http://localhost/fr/mosquee" >  /home/pi/Desktop/local_site.txt
 cp raspberry/upgrade.sh /home/pi/Desktop/Mawaqit
 chmod 777 /home/pi/Desktop/Mawaqit
 
