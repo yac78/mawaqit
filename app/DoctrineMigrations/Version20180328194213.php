52aafa6f184df00cf5e7afc062186209409ce27b
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2018-04-09 19:57:44 +0200

    update countries

---
+++ app/DoctrineMigrations/Version20180328194213.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180328194213 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('update mosque set country = upper(country)');
+        $this->addSql('update mosque set country = \'ALGERIE\' where country = \'ALGÉRIE\'');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
