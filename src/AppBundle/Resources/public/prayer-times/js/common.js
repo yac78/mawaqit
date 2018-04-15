c70e1967b26202bed0b55dde9db7b67677fbad42
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2018-04-15 19:00:54 +0200

    best full screeen handling fix css

--- src/AppBundle/Resources/public/prayer-times/js/common.js
+++ src/AppBundle/Resources/public/prayer-times/js/common.js
@@ -2,11 +2,27 @@ $('#full-screen i').click(function () {
     var self = $(this);
     if (self.hasClass('glyphicon-resize-full')) {
         fullscreen();
-        self.removeClass('glyphicon-resize-full');
-        self.addClass('glyphicon-resize-small');
     } else if (self.hasClass('glyphicon-resize-small')) {
         exitFullscreen();
-        self.removeClass('glyphicon-resize-small');
-        self.addClass('glyphicon-resize-full');
     }
-})
+    fullScreenHandler();
+});
+
+function fullScreenHandler() {
+    $fullScreenButton = $('#full-screen i');
+    $fullScreenButton.removeClass('glyphicon-resize-full');
+    $fullScreenButton.removeClass('glyphicon-resize-small');
+    if (screen.height === window.innerHeight) {
+        $fullScreenButton.addClass('glyphicon-resize-small');
+    } else {
+        $fullScreenButton.addClass('glyphicon-resize-full');
+    }
+}
+
+fullScreenHandler();
+
+$(document).ready(function () {
+    $(window).on('resize', function () {
+        fullScreenHandler();
+    });
+});
