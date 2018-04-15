c06c42fde3d28d1b1b2ff241f0bd1133a8d96118
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2018-04-15 10:49:29 +0200

    best deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,13 +3,25 @@
 # Exit on first error
 set -e
 
-if [ $# -lt 2 ]; then
-    echo "env and tag are mandatory"
+if [ $# -lt 1 ]; then
+    echo "env is mandatory"
     exit;
 fi
 
 env=$1
-tag=$2
+tag="master"
+
+if [ "$env" != "pp" ] && [ "$env" != "prod" ] ; then
+    echo "wrong env $env"
+fi
+
+if [ "$env" == "prod" ]; then
+    if [ $# -lt 2 ]; then
+        echo "tag is mandatory"
+        exit;
+    fi
+    tag=$2
+fi
 
 echo -n "Are you sur you want to deploy $tag to $env (y/n)? "
 read answer
@@ -18,9 +30,12 @@ if echo "$answer" | grep -iq "^y" ;then
     server="137.74.45.69"
     port="1983"
 
-    git tag $tag -m "new release $2" || true
     git push
-    git push origin $tag
+
+    if [ "$tag" != "master" ]; then
+        git tag $tag -m "new release $2" || true
+        git push origin $tag
+    fi
 
     ssh -p $port mawaqit@$server 'bash -s' < deploy/install.sh $env $tag
 fi
