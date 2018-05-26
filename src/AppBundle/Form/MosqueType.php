65e929a1d205af4c64c79a483fc5d7f0b8959364
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2018-05-26 11:48:08 +0200

    disable iqama for home

--- src/AppBundle/Form/MosqueType.php
+++ src/AppBundle/Form/MosqueType.php
@@ -182,6 +182,7 @@ class MosqueType extends AbstractType
         if ($mosque->getType() === "home") {
             $mosque->setAddOnMap(false);
             $mosque->setStatus(Mosque::STATUS_VALIDATED);
+            $mosque->getConfiguration()->setIqamaEnabled(false);
         }
     }
 
