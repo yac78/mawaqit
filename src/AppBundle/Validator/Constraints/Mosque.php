337e6841456e1a414e5b2c2d4da9e4607e5289a0
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2018-05-26 18:01:18 +0200

    add max mosques constraint

--- src/AppBundle/Validator/Constraints/Mosque.php
+++ src/AppBundle/Validator/Constraints/Mosque.php
@@ -7,6 +7,7 @@ use Symfony\Component\Validator\Constraint;
 class Mosque extends Constraint {
 
     public $mandatoryAddrMsg = 'form.mosque.address_mandatory';
+    public $maxReachedMsg = 'form.mosque.max_reached';
 
     public function getTargets() {
         return self::CLASS_CONSTRAINT;
--- src/AppBundle/Validator/Constraints/MosqueValidator.php
+++ src/AppBundle/Validator/Constraints/MosqueValidator.php
@@ -2,12 +2,26 @@
 
 namespace AppBundle\Validator\Constraints;
 
+use AppBundle\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use AppBundle\Entity\Mosque;
 
 class MosqueValidator extends ConstraintValidator
 {
+
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    const LIMIT_INSTALL = 10;
     /**
      * @param Mosque $mosque
      * @param Constraint $constraint
@@ -16,6 +30,14 @@ class MosqueValidator extends ConstraintValidator
     {
         if ($mosque->getType() === Mosque::TYPE_MOSQUE && empty($mosque->getAddress())) {
             $this->context->buildViolation($constraint->mandatoryAddrMsg)->addViolation();
+        }
+
+        /**
+         * @var User $user
+         */
+        $user = $this->tokenStorage->getToken()->getUser();
+        if (!$user->isAdmin() && $user->getMosques()->count() >= self::LIMIT_INSTALL) {
+            $this->context->buildViolation($constraint->maxReachedMsg)->addViolation();
         }
     }
 }
