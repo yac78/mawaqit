e1725a65708daa90d633118291c39fff523627d7
Ibrahim ZEHHAF <izehhaf@keley-live.com>
2018-06-09 16:38:24 +0200

    best from validation

--- src/AppBundle/Validator/Constraints/MosqueValidator.php
+++ src/AppBundle/Validator/Constraints/MosqueValidator.php
@@ -2,11 +2,11 @@
 
 namespace AppBundle\Validator\Constraints;
 
+use AppBundle\Entity\Mosque;
 use AppBundle\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
-use AppBundle\Entity\Mosque;
 
 class MosqueValidator extends ConstraintValidator
 {
@@ -28,10 +28,6 @@ class MosqueValidator extends ConstraintValidator
      */
     public function validate($mosque, Constraint $constraint)
     {
-        if ($mosque->getType() === Mosque::TYPE_MOSQUE && empty($mosque->getAddress())) {
-            $this->context->buildViolation($constraint->mandatoryAddrMsg)->addViolation();
-        }
-
         /**
          * @var User $user
          */
