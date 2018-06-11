44ab00065b9ac8185827e82a5d1c7c5d86818e78
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2018-06-11 02:51:11 +0200

    fix bug

--- src/AppBundle/Resources/public/js/iframe_modal.js
+++ src/AppBundle/Resources/public/js/iframe_modal.js
@@ -1,5 +1,5 @@
 $(".iframe-button").click(function () {
-    var iframe = '<iframe src="' + $(this).data('url') + '" frameborder="0" scrolling="no" style="width: 360px; height: 600px;"></iframe>'
+    var iframe = '<iframe src="' + $(this).data('url') + '" frameborder="0" scrolling="no" style="width: 360px; height: 670px;"></iframe>'
     $('#iframe-modal #iframe-text').text(iframe);
     $('#iframe-modal #iframe-html').html(iframe);
     $('#iframe-modal').modal();
