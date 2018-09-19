ad647e67035682bb7eabe0c64d430c0dcf729167
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2018-09-19 21:59:52 +0200

    fix bug

--- raspberry/run.sh
+++ raspberry/run.sh
@@ -10,7 +10,7 @@ sleep 10;
 wget -q --spider $url
 
 if [ $? -ne 0 ]; then
-    url=http://mawaqit.local/mosque
+    url=http://mawaqit.local/mosquee
 fi
 
 chromium-browser --app=$url --start-fullscreen --start-maximized
