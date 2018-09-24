ca0ca2ee508cf6e2e8f0beffb4f5b1028c88fd9d
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2018-09-24 21:22:39 +0200

    fix message display time

---
+++ app/DoctrineMigrations/Version20180923165441.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180923165441 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('UPDATE configuration set time_to_display_message = 60 where time_to_display_message > 60');
+        $this->addSql('UPDATE configuration set time_to_display_message = 20 where time_to_display_message < 5');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE configuration ADD azan_bip TINYINT(1) NOT NULL, ADD iqama_bip TINYINT(1) NOT NULL');
+    }
+}
