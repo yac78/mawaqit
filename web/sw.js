c2012255f88a7c1f6904eb7d9d232b50545cc16e
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2018-12-21 09:24:01 +0100

    PWA

--- web/sw.js
+++ web/sw.js
@@ -1,11 +1,36 @@
 importScripts('sw-toolbox.js');
 
-toolbox.precache([
-   '/fr/mosquee-essunna-houilles',
-]);
-
-toolbox.router.get(/\.(?:js|css|png|jpg)$/, toolbox.cacheFirst, {
+toolbox.router.get(/.*/, toolbox.networkFirst, {
     cache: {
-        name: 'assets'
+        name: 'mawaqit',
     }
 });
+
+// Notification.requestPermission(function(status) {
+//     console.log('Notification permission status:', status);
+// });
+//
+// function displayNotification() {
+//     if (Notification.permission == 'granted') {
+//         navigator.serviceWorker.getRegistration().then(function(reg) {
+//             reg.showNotification('Hello world!');
+//         });
+//     }
+// }
+//
+// function displayNotification() {
+//     if (Notification.permission == 'granted') {
+//         navigator.serviceWorker.getRegistration().then(function(reg) {
+//             var options = {
+//                 body: 'Here is a notification body!',
+//                 icon: 'android-chrome-192x192.png',
+//                 vibrate: [100, 50, 100],
+//                 data: {
+//                     dateOfArrival: Date.now(),
+//                     primaryKey: 1
+//                 }
+//             };
+//             reg.showNotification('Hello world!', options);
+//         });
+//     }
+// }
