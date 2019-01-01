de6616abad260b07957261604ae04c7e7092fced
Ibrahim ZEHHAF <izehhaf@keley-live.com>
2019-01-01 19:26:27 +0100

    test

--- src/AppBundle/Controller/Admin/TestController.php
+++ src/AppBundle/Controller/Admin/TestController.php
@@ -2,15 +2,12 @@
 
 namespace AppBundle\Controller\Admin;
 
-use AppBundle\Entity\Mosque;
-use AppBundle\Form\HijriAdjustmentType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * @Route("/admin/test", options={"i18n"="false"})
+ * @Route("/test", options={"i18n"="false"})
  */
 class TestController extends Controller
 {
