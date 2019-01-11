54925a4ae1d23cff124d1a21888f47d3e49be1e7
Ibrahim ZEHHAF <izehhaf@keley-live.com>
2019-01-11 14:06:40 +0100

    test new scripts for docker install prod

--- deploy/install-docker.sh
+++ deploy/install-docker.sh
@@ -18,21 +18,13 @@ docker exec $dockerContainer git fetch && git checkout $tag
 #echo "Creating symlinks"
 #ln -snf $sharedDir/upload/ $targetDir/web/upload
 #ln -snf $sharedDir/static $targetDir/web/static
-#ln -snf $sharedDir/parameters.$env.yml $targetDir/app/config/parameters.yml
 #ln -snf $sharedDir/robots.txt.$env $targetDir/web/robots.txt
-
-#if [ "$env" == "prod" ]; then
-#    ln -snf $sharedDir/logs $targetDir/var/logs
-#    ln -snf $sharedDir/sessions $targetDir/var/sessions
-#fi
-
 
 echo "Set version"
 docker exec $dockerContainer sed -i "s/version: .*/version: $tag/" app/config/parameters.yml
 
 # install vendors and assets
-docker exec $dockerContainer bash -c "export SYMFONY_ENV=prod"
-docker exec $dockerContainer composer install -on # --no-dev
+docker exec $dockerContainer bash -c "SYMFONY_ENV=prod composer install -on --no-dev"
 docker exec $dockerContainer bin/console assets:install --env=prod --no-debug
 docker exec $dockerContainer bin/console assetic:dump --env=prod --no-debug
 
@@ -43,10 +35,10 @@ docker exec $dockerContainer bin/console assetic:dump --env=prod --no-debug
 #fi
 
 # migrate DB
-docker exec $dockerContainer bin/console doc:mig:mig -n --allow-no-migration
+docker exec $dockerContainer bash -c "bin/console doc:mig:mig -n --allow-no-migration"
 
 echo "Reset opcache"
-curl -s localhost/reset_opcache.php
+#curl -s localhost/reset_opcache.php
 
 echo ""
 echo "####################################################"
