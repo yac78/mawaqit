113286b05aa5e74cfd9c3cb07bf5909003f3a868
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-01-12 19:19:45 +0100

    Test new nginx conf

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 set -e
-if [ -f /var/www/app/console ]; then
-    echo "Initialize Symfony 3"
 
-    cd /var/www/
-    setfacl -R -m u:www-data:rwx -m u:`whoami`:rwx var/cache var/logs var/sessions web/*/ || true
-    setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx var/cache var/logs var/sessions web/*/ || true
-fi
+php-fpm7 -D
+nginx -g 'daemon off;'
+
+chown -R www-data:www-data /var/www/var/cache
+chown -R www-data:www-data /var/www/var/logs
+chown -R www-data:www-data /var/www/var/sessions
