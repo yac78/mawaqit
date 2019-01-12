f5b7396161e532e91a3513435ac7f5bc22093be3
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-01-12 23:24:15 +0100

    fix raspberry update script

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-chown -R nobody:nobody /var/www/var/cache
-chown -R nobody:nobody /var/www/var/logs
-chown -R nobody:nobody /var/www/var/sessions
+chown -R nobody:nobody /var/www/mawaqit/var/cache
+chown -R nobody:nobody /var/www/mawaqit/var/logs
+chown -R nobody:nobody /var/www/mawaqit/var/sessions
 
 php-fpm7 -D
 nginx -g 'daemon off;'
