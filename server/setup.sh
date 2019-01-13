6a3a79fc01469c993136a2e1c26659d106d8e195
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-01-13 17:54:45 +0100

    Test new docker conf

---
+++ server/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+apt-get update
+apt-get install rsync git acl
+
+# install Docker CE
+apt-get apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce
