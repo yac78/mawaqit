825e8eed1750d4bae27a46c9091e5582e43c42cf
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-01-13 10:17:33 +0100

    Test new docker conf

--- docker/start.sh
+++ docker/start.sh
@@ -3,4 +3,4 @@
 php-fpm7 -D
 nginx -g 'daemon off;'
 
-while :; do sleep 6h & wait $${!}; nginx -s reload; done & nginx -g 'daemon off;'
+#while :; do sleep 6h & wait $${!}; nginx -s reload; done & nginx -g 'daemon off;'
