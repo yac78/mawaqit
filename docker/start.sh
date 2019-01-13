6bee805fc7fd671f77ed848ed6982361a13a1889
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-01-13 10:06:34 +0100

    Test new docker conf

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 set -e
 
-chown -R nobody:nobody /var/www/mawaqit/var/cache
-chown -R nobody:nobody /var/www/mawaqit/var/logs
-chown -R nobody:nobody /var/www/mawaqit/var/sessions
-
 php-fpm7 -D
 nginx -g 'daemon off;'
