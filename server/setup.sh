75575611539806e9234329e684f388a5521611a3
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-01-13 18:13:23 +0100

    Test new docker conf

--- server/setup.sh
+++ server/setup.sh
@@ -11,3 +11,8 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update
 apt-get install docker-ce
+
+# Secure server
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+echo "AddressFamily inet" >> /etc/ssh/sshd_config
