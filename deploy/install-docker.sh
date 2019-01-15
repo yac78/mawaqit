e73b43970d7e57f3fbafbe2276865a7964203fd6
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-01-15 07:52:12 +0100

    update deploy script

--- deploy/install-docker.sh
+++ deploy/install-docker.sh
@@ -5,24 +5,22 @@ env=$1
 tag=$2
 baseDir=/var/www/mawaqit
 repoDir=$baseDir/repo
+dockerContainer=mawaqit
 
 cd $repoDir
 
-dock git fetch && git checkout $tag
+docker exec $dockerContainer git fetch && git checkout $tag
 
 echo "Creating symlinks"
-dock sh -c "(cd web && ln -snf robots.txt.$env robots.txt)"
+docker exec $dockerContainer sh -c "(cd web && ln -snf robots.txt.$env robots.txt)"
 
 echo "Set version"
-dock sed -i "s/version: .*/version: $tag/" app/config/parameters.yml
+docker exec $dockerContainer sed -i "s/version: .*/version: $tag/" app/config/parameters.yml
 
 # Install vendors and assets
-dock sh -c "SYMFONY_ENV=prod composer install -on --no-dev"
-dock bin/console assets:install -e prod --no-debug
-dock bin/console assetic:dump -e prod --no-debug
-
-# Fix permissions
-dock chmod -R 777 var/cache var/logs var/sessions
+docker exec $dockerContainer sh -c "SYMFONY_ENV=prod composer install -on --no-dev"
+docker exec $dockerContainer bin/console assets:install -e prod --no-debug
+docker exec $dockerContainer bin/console assetic:dump -e prod --no-debug
 
 # Sync DB if prod deploy
 if [ "$env" == "prod" ]; then
@@ -31,10 +29,10 @@ if [ "$env" == "prod" ]; then
 fi
 
 # Migrate DB
-dock bin/console doc:mig:mig -n --allow-no-migration -e prod
+docker exec $dockerContainer bin/console doc:mig:mig -n --allow-no-migration -e prod
 
 # Restart php
-dock kill -USR2 1
+docker exec $dockerContainer kill -USR2 1
 
 echo ""
 echo "####################################################"
