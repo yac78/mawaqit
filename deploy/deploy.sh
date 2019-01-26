ae36f5a0cd9d02ff1700b18a187a5ec040b44b54
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-01-26 12:01:13 +0100

    update deploy scripts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -14,17 +14,7 @@ branch=$(git branch | grep \* | cut -d ' ' -f2)
 git pull origin master
 git push origin $branch
 
-if [ "$env" == "prod" ]; then
-    echo -n "Are you sur you want to deploy $branch to $env (y/n)? "
-    read answer
-else
-    answer=y
-fi
+server="137.74.45.69"
+port="1983"
 
-
-if echo "$answer" | grep -iq "^y" ;then
-    server="137.74.45.69"
-    port="1983"
-
-    ssh -p $port mawaqit@$server 'bash -s' < deploy/install.sh $env $branch
-fi
+ssh -p $port mawaqit@$server 'bash -s' < deploy/install.sh $env $branch
--- deploy/install.sh
+++ deploy/install.sh
@@ -12,50 +12,26 @@ env=$1
 branch=$2
 baseDir=/var/www/mawaqit
 repoDir=$baseDir/repo
-sharedDir=$baseDir/shared
-envDir=$baseDir/$env
-targetDir=$envDir/master
 
 cd $repoDir
 
-if [ "$env" == "prod" ]; then
-    git fetch && git checkout $branch && git pull origin $branch
-fi
-
-version=dev@`git rev-parse --short HEAD`
-
-mkdir -p $targetDir
-
-echo "Copying files"
-rsync -r --delete --files-from=$repoDir/deploy/files-to-include --exclude-from=$repoDir/deploy/files-to-exclude $repoDir $targetDir
+git checkout $branch && git pull origin $branch
 
 echo "Creating symlinks"
-ln -snf $sharedDir/upload/ $targetDir/web/upload
-ln -snf $sharedDir/static $targetDir/web/static
-ln -snf $sharedDir/parameters.$env.yml $targetDir/app/config/parameters.yml
-ln -snf $sharedDir/robots.txt.$env $targetDir/web/robots.txt
-
-if [ "$env" == "prod" ]; then
-    ln -snf $sharedDir/logs $targetDir/var/logs
-    ln -snf $sharedDir/sessions $targetDir/var/sessions
-fi
-
-cd $targetDir
+ln -snf $repoDir/web/robots.txt.$env $repoDir/web/robots.txt
 
 echo "Set version"
+version=dev@`git rev-parse --short HEAD`
 sed -i "s/version: .*/version: $version/" app/config/parameters.yml
 
 # install vendors and assets
 export SYMFONY_ENV=prod
-composer install --no-dev --optimize-autoloader --no-interaction
-bin/console assets:install --env=prod --no-debug
-bin/console assetic:dump --env=prod --no-debug
+composer install --no-dev -n -o
+bin/console assets:install -e prod --no-debug
+bin/console assetic:dump -e prod --no-debug
 
 # migrate DB
-bin/console doctrine:migrations:migrate -n --allow-no-migration
-
-echo "Creating current symlink"
-cd $envDir && rm current || true && ln -s $targetDir current
+bin/console doctrine:migrations:migrate -n --allow-no-migration -e prod
 
 echo "Reset opcache"
 curl -s localhost:81/reset_opcache.php
