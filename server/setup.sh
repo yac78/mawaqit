7ebd113c09000d012f523456fae88677debe6988
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-01-26 16:59:50 +0100

    pp setup

--- server/setup.sh
+++ server/setup.sh
@@ -2,26 +2,25 @@
 
 set -e
 
-apt update
-apt update
-apt upgrade
-apt dist-upgrade
-apt install rsync git acl
+sud oapt update
+sudo apt upgrade
+sudo apt dist-upgrade
+sudo apt install rsync git acl
 
 # install Docker CE
-apt apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt install  apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-add-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-apt update
-apt install docker-ce
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian  $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce
 
 # install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 # Secure server
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
-echo "AddressFamily inet" >> /etc/ssh/sshd_config
+sudo echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+sudo echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+sudo echo "AddressFamily inet" >> /etc/ssh/sshd_config
 
 # Customise .bashrc
 echo "cd /var/www/mawaqit" >> ~/.bashrc
