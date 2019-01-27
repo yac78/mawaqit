501b10196d1f29a8ab4c7a2c58e48494da32a027
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-01-27 10:39:05 +0100

    update deploy scripts

--- deploy/install.sh
+++ deploy/install.sh
@@ -6,17 +6,18 @@ tag=$2
 baseDir=/var/www/mawaqit
 repoDir=$baseDir/repo
 dockerContainer=mawaqit
+dockerUser=mawaqit:mawaqit
 
 cd $repoDir
 
-docker exec $dockerContainer git fetch && git checkout $tag
+docker exec --user $dockerUser $dockerContainer git fetch && git checkout $tag
 
 if [ "$env" == "pp" ]; then
-    docker exec $dockerContainer git pull origin $tag
+    docker exec --user $dockerUser $$dockerContainer git pull origin $tag
 fi
 
 echo "Creating symlinks"
-docker exec $dockerContainer sh -c "(cd web && ln -snf robots.txt.$env robots.txt)"
+docker exec --user $dockerUser $dockerContainer sh -c "(cd web && ln -snf robots.txt.$env robots.txt)"
 
 echo "Set version"
 version=dev@`git rev-parse --short HEAD`
@@ -24,18 +25,18 @@ if [ "$env" == "prod" ]; then
     version=$tag
 fi
 
-docker exec $dockerContainer sed -i "s/version: .*/version: $version/" app/config/parameters.yml
+docker exec --user $dockerUser $dockerContainer sed -i "s/version: .*/version: $version/" app/config/parameters.yml
 
 # Install vendors and assets
-docker exec $dockerContainer sh -c "SYMFONY_ENV=prod composer install -o -n --no-dev"
-docker exec $dockerContainer bin/console assets:install -e prod --no-debug
-docker exec $dockerContainer bin/console assetic:dump -e prod --no-debug
+docker exec --user $dockerUser $dockerContainer sh -c "SYMFONY_ENV=prod composer install -o -n --no-dev"
+docker exec --user $dockerUser $dockerContainer bin/console assets:install -e prod --no-debug
+docker exec --user $dockerUser $dockerContainer bin/console assetic:dump -e prod --no-debug
 
 # Migrate DB
-docker exec $dockerContainer bin/console doc:mig:mig -n --allow-no-migration -e prod
+docker exec --user $dockerUser $dockerContainer bin/console doc:mig:mig -n --allow-no-migration -e prod
 
 # Restart php
-docker exec $dockerContainer kill -USR2 1
+docker exec --user $dockerUser $dockerContainer kill -USR2 1
 
 # Sync DB if prod deploy
 if [ "$env" == "prod" ]; then
