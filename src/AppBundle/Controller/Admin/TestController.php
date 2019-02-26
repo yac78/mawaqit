c5063c849d2b05c36c7a3aa66c131715db18b49d
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-02-26 22:55:36 +0100

    new email design

--- src/AppBundle/Controller/Admin/TestController.php
+++ src/AppBundle/Controller/Admin/TestController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Entity\Mosque;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,6 +19,17 @@ class TestController extends Controller
     public function testAllAction()
     {
         return new Response('ok', 500);
+    }
+
+    /**
+     * @Route("/mail-preview/{template}/{id}")
+     */
+    public function mailPreviewAction($template, Mosque $mosque)
+    {
+        return $this->render(":email_templates:$template.html.twig", [
+            "mosque" => $mosque,
+            "content" => 'toto'
+        ]);
     }
 
 }
