2e7c557ef6e53036b3b44418f0c3ed8f8f540fd8
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-03-10 11:51:12 +0100

    fix docker conf

--- deploy/install.sh
+++ deploy/install.sh
@@ -6,7 +6,6 @@ tag=$2
 baseDir=/var/www/mawaqit
 repoDir=$baseDir/repo
 dockerContainer=mawaqit
-dockerUser=1001:1001
 
 cd $repoDir
 
@@ -19,14 +18,14 @@ if [ "$env" == "prod" ]; then
     $baseDir/tools/dbSync.sh
 fi
 
-docker exec --user $dockerUser $dockerContainer git fetch && git checkout $tag
+docker exec  $dockerContainer git fetch && git checkout $tag
 
 if [ "$env" == "pp" ]; then
-    docker exec --user $dockerUser $dockerContainer git pull origin $tag
+    docker exec $dockerContainer git pull origin $tag
 fi
 
 echo "Creating symlinks"
-docker exec --user $dockerUser $dockerContainer sh -c "(cd web && ln -snf robots.txt.$env robots.txt)"
+docker exec $dockerContainer sh -c "(cd web && ln -snf robots.txt.$env robots.txt)"
 
 echo "Set version"
 version=dev@`git rev-parse --short HEAD`
@@ -37,12 +36,12 @@ fi
 docker exec --user $dockerUser $dockerContainer sed -i "s/version: .*/version: $version/" app/config/parameters.yml
 
 # Install vendors and assets
-docker exec --user $dockerUser $dockerContainer sh -c "SYMFONY_ENV=$env composer install -o -n --no-dev"
-docker exec --user $dockerUser $dockerContainer bin/console assets:install -e $env --no-debug
-docker exec --user $dockerUser $dockerContainer bin/console assetic:dump -e $env --no-debug
+docker exec $dockerContainer sh -c "SYMFONY_ENV=$env composer install -o -n --no-dev"
+docker exec $dockerContainer bin/console assets:install -e $env --no-debug
+docker exec $dockerContainer bin/console assetic:dump -e $env --no-debug
 
 # Migrate DB
-docker exec --user $dockerUser $dockerContainer bin/console doc:mig:mig -n --allow-no-migration -e $env
+docker exec $dockerContainer bin/console doc:mig:mig -n --allow-no-migration -e $env
 
 # Restart php
 docker exec $dockerContainer kill -USR2 1
