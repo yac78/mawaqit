f7911f4de1fb067fa23fcf871e2f419b4ee32134
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-09-22 12:12:57 +0200

    new infra orga for pp

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 ln -sf docker-compose.dev.yml docker-compose.yml
 sudo rm -rf var/logs/* var/cache/* var/sessions/*
 docker-compose kill && docker-compose rm -f && docker-compose up -d --build
-docker-compose exec php-fpm  chmod 777 -R var/logs var/cache var/sessions
+docker-compose exec mawaqit_php  chmod 777 -R var/logs var/cache var/sessions
 sleep 10
-docker-compose exec php-fpm bin/console d:s:u -f
-docker-compose exec php-fpm bin/console h:f:l -n
+docker-compose exec mawaqit_php bin/console d:s:u -f
+docker-compose exec mawaqit_php bin/console h:f:l -n
 
 echo "------------------------------------------"
 echo "Install OK"
-echo "Site: http://localhost:8101"
-echo "Maildev: http://localhost:8103"
+echo "Site: http://localhost:10001"
+echo "Maildev: http://localhost:10003"
 echo "------------------------------------------"
