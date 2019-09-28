e1a90524d5c7da37fe293f7db385b7124b37a9f5
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-09-28 21:42:47 +0200

    change reminder start date

--- src/AppBundle/Repository/MosqueRepository.php
+++ src/AppBundle/Repository/MosqueRepository.php
@@ -293,7 +293,7 @@ class MosqueRepository extends EntityRepository
             ->andWhere("m.image3 IS NULL")
             ->andWhere("m.created > :date")
             ->andWhere("m.emailScreenPhotoReminder IS NULL OR m.emailScreenPhotoReminder <= 3")
-            ->setParameter(":date", new DateTime("2019-09-27"))
+            ->setParameter(":date", new DateTime("2019-09-28"))
             ->getQuery()
             ->execute();
     }
