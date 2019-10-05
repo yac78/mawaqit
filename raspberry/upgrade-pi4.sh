756f8c7f005dee9102591c16edeb6c25a9c190b4
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-10-05 17:55:30 +0200

    Update upgrade-pi4.sh

--- raspberry/upgrade-pi4.sh
+++ raspberry/upgrade-pi4.sh
@@ -15,20 +15,15 @@ currenttag=$(git describe --tags --abbrev=0)
 latesttag=$(git tag | sort -V | tail -1)
 
 if [ "$currenttag" != "$latesttag" ]; then
-
     echo checking out ${latesttag}
     git checkout ${latesttag}
-
     version=`echo $latesttag | sed 's/-.*//'`
     sed -i "s/version: .*/version: $version/" app/config/parameters.yml
-    sudo rm -rf /tmp/*
-    sudo rm -rf var/cache/* var/logs/*
+    sudo rm -rf /tmp/* var/cache/* var/logs/*
     docker-compose run mawaqit_composer sh -c "export SYMFONY_ENV=raspberry; composer install -o -n --no-dev"
-    sudo rm -rf var/cache/* var/logs/*
     docker-compose exec mawaqit_php bin/console assets:install --env=raspberry --no-debug
     docker-compose exec mawaqit_php bin/console assetic:dump --env=raspberry --no-debug
     docker-compose exec mawaqit_php sh -c "export SYMFONY_ENV=raspberry; bin/console doc:mig:mig -n --allow-no-migration"
-    sudo rm -rf var/cache/* var/logs/* 
 else
     echo "You are on the last version :)"
 fi
