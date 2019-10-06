007eb754805b7cb83045189ce67504b8b0c7304d
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-10-06 19:13:01 +0200

    impove install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 ln -sf docker-compose.dev.yml docker-compose.yml
-mkdir -p var/logs/ var/cache/ var/sessions/
-sudo rm -rf var/logs/* var/cache/* var/sessions/*
 docker-compose kill && docker-compose rm -f && docker-compose up -d --build
-docker-compose exec mawaqit_php  chmod 777 -R var/logs var/cache var/sessions
 sleep 10
 docker-compose exec mawaqit_php bin/console d:s:u -f
 docker-compose exec mawaqit_php bin/console h:f:l -n
 
+mkdir -p var/logs/ var/cache/ var/sessions/
+sudo rm -rf var/logs/* var/cache/* var/sessions/*
+docker-compose exec mawaqit_php chmod -R 777 var/logs var/cache var/sessions
+
 echo "------------------------------------------"
 echo "Install OK"
-echo "Site: http://localhost:10001"
+echo "Site > http://localhost:10001  Credentials > login: local@local.com  password: local"
 echo "Maildev: http://localhost:10003"
 echo "------------------------------------------"
