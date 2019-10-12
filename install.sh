8dc3ac54ca2d950693010788e94603db8270dbe6
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-10-12 11:02:19 +0200

    fix install script

--- install.sh
+++ install.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
 ln -sf docker-compose.dev.yml docker-compose.yml
-docker-compose kill && docker-compose rm -f && docker-compose up -d --build
-sleep 10
-docker-compose exec mawaqit_php bin/console d:s:u -f
-docker-compose exec mawaqit_php bin/console h:f:l -n
+docker-compose down && docker-compose up -d --build
 
-mkdir -p var/logs/ var/cache/ var/sessions/
+docker-compose exec mawaqit_php mkdir -p var/logs var/cache var/sessions
 docker-compose exec mawaqit_php rm -rf var/logs/* var/cache/* var/sessions/*
 docker-compose exec mawaqit_php chmod -R 777 var/logs var/cache var/sessions
 
+echo ""
+echo "Waiting for database..."
+
+while ! docker-compose exec mawaqit_mysql mysqladmin ping -h"127.0.0.1" --silent; do
+    sleep 1
+done
+
+docker-compose exec mawaqit_php bin/console d:s:u -f
+docker-compose exec mawaqit_php bin/console h:f:l -n
+
 echo "------------------------------------------"
-echo "Install OK"
-echo "Site > http://localhost:10001  Credentials > login: local@local.com  password: local"
+echo "Mawaqit is up"
+echo "http://localhost:10001 / login: local@local.com / password: local"
+echo "Database: host 127.0.0.1, port 10002, user root, password mawaqit"
 echo "Maildev: http://localhost:10003"
 echo "------------------------------------------"
