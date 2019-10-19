9d1a81bf993f3a3e7161f1054d6ece475f295fde
Ibrahim ZEHHAF <ibrahim.zehhaf.pro@gmail.com>
2019-10-19 15:54:27 +0200

    Update run.sh

--- raspberry/run.sh
+++ raspberry/run.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
 # online site
-url=https://mawaqit.net/fr/mosquee-essunna-houilles
 if [ -f ~/Desktop/online_site.txt ]; then
     url=`cat ~/Desktop/online_site.txt`
 fi
 
-i=0
-while ! wget -q --timeout=2 --spider $url; do
-    sleep 1
-    i=$(( $i + 1 ))
-    if (( $i == 10 )); then
-       url=http://mawaqit.local/mosquee
-       break
-    fi
-done
+sleep 10;
+
+wget -q --spider $url
+
+if [ $? -ne 0 ]; then
+    url=http://mawaqit.local/mosquee
+fi
+
+#i=0
+#while ! wget -q --spider --timeout=2 $url; do
+#  sleep 1
+#  i=$(( $i + 1 ))
+#  if (( $i == 3 )); then
+#    url=http://mawaqit.local/mosquee
+#    
+#    # check if local server is responding
+#    i=0
+#    while ! wget -q --spider --timeout=1 $url; do
+#      sleep 1
+#      i=$(( $i + 1 ))
+#      if (( $i == 30 )); then
+#        break
+#      fi
+#    done
+#    break
+#  fi
+#done
 
 chromium-browser --app=$url --start-fullscreen --start-maximized
